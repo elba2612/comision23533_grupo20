9caf0aeb0216f2402273cbefab4956fc590b6375
Lucas Igarzabal <igarzaballucas@hotmail.com>
2023-10-27 11:37:19 -0300

    un poco de todo

--- JavaScript/script_index.js
+++ JavaScript/script_index.js
@@ -18,60 +18,65 @@ document.getElementById('menu-hamburguesa').addEventListener('click', () => {
 })
 
 document.body.addEventListener('click', function (e) {
-    let ids = [];
-    ids.push(e.target);
-    ids.push(e.target.parentNode);
-    ids.push(e.target.parentNode.parentNode);
-    let esId = false;
-    let esClass = false;
-    for (const id of ids) {
-        if (id.id == 'menu-hamburguesa') {
-            esId = true;
+    if (!wide.matches) {
+        let ids = [];
+        ids.push(e.target);
+        ids.push(e.target.parentNode);
+        ids.push(e.target.parentNode.parentNode);
+        let esId = false;
+        let esClass = false;
+        for (const id of ids) {
+            if (id.id == 'menu-hamburguesa') {
+                esId = true;
+            }
+            if (id.classList && id.classList.contains('boton-nav')) {
+                esClass = true;
+            }
         }
-        if (id.classList && id.classList.contains('boton-nav')) {
-            esClass = true;
+        let nav = document.getElementsByTagName('nav');
+        let menu = document.getElementsByClassName('menu');
+        if ((!esClass && menu[0].classList.contains('visible')) || (esId && menu[0].classList.contains('visible'))) {
+            menu[0].classList.remove('visible');
+            nav[0].classList.remove('para-hamburguesa');
+            menu[0].style.position = 'absolute';
+            menu[0].style.display = 'none';
+        } else if (esId && !menu[0].classList.contains('visible')) {
+            menu[0].style.display = 'flex';
+            sleep(50).then(() => {
+                nav[0].classList.toggle('para-hamburguesa');
+                menu[0].classList.add('visible');
+                menu[0].style.position = 'static';
+            })
         }
     }
-    let nav = document.getElementsByTagName('nav');
-    let menu = document.getElementsByClassName('menu');
-    if ((!esClass && menu[0].classList.contains('visible')) || (esId && menu[0].classList.contains('visible'))) {
-        menu[0].classList.remove('visible');
-        nav[0].classList.remove('para-hamburguesa');
-        menu[0].style.position = 'absolute';
-        menu[0].style.display = 'none';
-    } else if (esId && !menu[0].classList.contains('visible')) {
-        menu[0].style.display = 'flex';
-        sleep(50).then(() => {
-            nav[0].classList.toggle('para-hamburguesa');
-            menu[0].classList.add('visible');
-            menu[0].style.position = 'static';
-        })
-    }
+
 });
 
+var myHeaders = new Headers();
+myHeaders.append("x-access-token", "goldapi-dxjqzrlo8nrtiu-io");
+myHeaders.append("Content-Type", "application/json");
 
-
-
-const url = 'https://metalpriceapi.p.rapidapi.com/v1/latest?api_key=b2685b54d7c7d0313f1d05e8a8dc7761&base=USD&currencies=XAU';
-const options = {
+var requestOptions = {
     method: 'GET',
-    headers: {
-        'X-RapidAPI-Key': 'e7639d8764msh3ecaf10d2104778p1b0ca0jsnff915543a065',
-        'X-RapidAPI-Host': 'metalpriceapi.p.rapidapi.com'
-    }
+    headers: myHeaders,
+    redirect: 'follow'
 };
+let apiFuncionando = false;
 
-getPrice();
-async function getPrice() {
-    try {
-        const response = await fetch(url, options);
-        const result = JSON.parse(await response.text());
-        console.log(result.rates.XAU);
-        document.querySelector('#precio').textContent=`Precio del oro: $${(1/result.rates.XAU).toFixed(2)} por onza de oro`;
-    } catch (error) {
-        console.error(error);
-    }
+if (apiFuncionando) {
+    fetch("https://www.goldapi.io/api/XAU/USD", requestOptions)
+        .then(response => response.text())
+        .then(result => {
+            const res = JSON.parse(result)
+            console.log(res)
+            document.querySelector('#precio').textContent = `Precio del oro: $${(res.open_price).toFixed(2)} por onza de oro`;
+        })
+        .catch(error => console.log('error', error));
+} else {
+    document.querySelector('#precio').textContent = `Precio del oro: $xxxx.xx por onza de oro`;
 }
+
+
 
 
 
